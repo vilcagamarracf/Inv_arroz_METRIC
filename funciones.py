54eec95b279784b88fca1c9810b737e4e78a89b9
Cesar Vilca <vilcagamarracf@gmail.com>
2021-12-06 13:25:27 -0500

    funciones file added

--- funciones.py
+++ funciones.py
@@ -1,55 +1,76 @@
 # Funciones usadas en Inv-Arroz-METRIC
 
-#------------- LANDSAT 8 ------------- 
-def generar_reporte_landsat8_T1_SR(path, row, year):
+import ee
+import geemap
+import pandas as pd
+from ipywidgets import interact, fixed
 
-  ID_snippet_name = "LANDSAT/LC08/C01/T1_SR"
-  ## Filtrado de ee.imageCollection
-  icol_sr = (
-      ee.ImageCollection(ID_snippet_name)\
-      .filterDate(str(year), str(year+1))\
-      .filterMetadata('WRS_PATH', 'equals', path)\
-      .filterMetadata('WRS_ROW', 'equals', row)
-  )
+# Función generar_reporte(icol)
 
+def generar_reporte(icol):
+  """Generar reportes para ee.ImageCollection 's
+  
+  A partir de un ee.ImageCollection devolver una tabla 
+  con información de ID, Fechas (Año, Mes, Día, Día Juliano) y 
+  propiedades como Ángulo Zenital y Porcentaje de Nubosidad.
+
+  Argumentos:
+  - icol : ee.ImageCollection
+  Retorna:
+  - pandas.Dataframe
+  """
   ## Generando campos para la tabla
+  ID_snippet_name = icol.getInfo()['id']
+
   # Lista con fechas (en milisegundos)
-  lista_fechas = icol_sr.aggregate_array('system:time_start').getInfo()
-
+  lista_fechas = icol.aggregate_array('system:time_start').getInfo() # Fechas
   # Lista con ID's
-  imgCol_ids = [f'{ID_snippet_name}/{i}' 
-                for i in icol_sr.aggregate_array("system:index").getInfo()]
+  imgCol_ids = [f'{ID_snippet_name}/{i}' for i in icol.aggregate_array("system:index").getInfo()] # ID
 
-  # Tabla con pandas
-  # import pandas as pd
-
+  # Armando dataframe
   df = pd.DataFrame(lista_fechas, columns = ['millis'])
 
-  df["Landsat ID"] = imgCol_ids
+  df["ID GEE"] = imgCol_ids
   df['Timestamp'] = pd.to_datetime(df['millis'], unit='ms')
   df['Fecha Precisa'] = pd.DatetimeIndex(df['Timestamp']) # Con hora
-  df['Fecha Corta'] = pd.DatetimeIndex(df['Timestamp']).date
+  # df['Fecha Corta'] = pd.DatetimeIndex(df['Timestamp']).date
   df['Año'] = pd.DatetimeIndex(df['Timestamp']).year
   df['Mes'] = pd.DatetimeIndex(df['Timestamp']).month
   df['Día'] = pd.DatetimeIndex(df['Timestamp']).day
-  df['Hora'] = pd.DatetimeIndex(df['Timestamp']).hour
+  # df['Hora'] = pd.DatetimeIndex(df['Timestamp']).hour
   df['Día Juliano'] = pd.DatetimeIndex(df['Timestamp']).dayofyear
-  # df['Sensor'] = 'OLI/TIRS'
+
+  # Agregando propiedades
+  if ID_snippet_name == 'LANDSAT/LC08/C01/T1_SR':
+    df["Zenith Angle"] = icol.aggregate_array('SOLAR_ZENITH_ANGLE').getInfo()
+    df["Porcentaje Nubes (%)"] = icol.aggregate_array('CLOUD_COVER').getInfo()
   
-  df["Zenith Angle"] = icol_sr.aggregate_array('SOLAR_ZENITH_ANGLE').getInfo()
-  df["Porcentaje Nubes (%)"] = icol_sr.aggregate_array('CLOUD_COVER').getInfo()
+  elif ID_snippet_name == 'COPERNICUS/S2_SR':
+    df["Zenith Angle"] = icol.aggregate_array('MEAN_SOLAR_ZENITH_ANGLE').getInfo()
+    df["Porcentaje Nubes (%)"] = icol.aggregate_array('CLOUDY_PIXEL_PERCENTAGE').getInfo()
   
   df = df.drop(columns=['millis', 'Timestamp'])
   # df.to_csv('datos_2020_L8_SR.csv')
   
   return df
 
-# @markdown Función `visualizar_mes_L8_SR(mes, df, roi)`
 
-def visualizar_mes_L8_SR(mes, df, roi):
+# Función ver_imgs_mensual(mes, df, roi)
+
+def ver_imgs_mensual(mes, df, roi):
+  """Visualizar imágenes a partir del reporte generado con la función generar_reporte
   
+  Argumentos:
+  - mes : lista de valores [1,2,3] del 1 al 12 que representen los meses
+  - df : pandas.DataFrame con columna 'ID GEE' de ID's 
+  - roi : ee.geometry o ee.FeatureCollection 
+  Retorna:
+  - ee.Image
+
+  Usarse con interact
+  """
   # Del dataframe obtenemos las imagenes de acuerdo al mes que queramos
-  lista_imagenes = df[df['Mes'] == mes]['Landsat ID'].tolist()
+  lista_imagenes = df[df['Mes'] == mes]['ID GEE'].tolist()
 
   # Parametros de visualización RGB
   vis_rgb = {'min': 0.0, 'max': 0.3, 'bands': ['B4', 'B3', 'B2']}
@@ -57,63 +78,23 @@ def visualizar_mes_L8_SR(mes, df, roi):
   
   # geemap para la visualización
   Map = geemap.Map(basemap='OpenStreetMap.Mapnik', layer_ctrl=True)
-
   Map.centerObject(roi, 12)  # Map.setCenter(-79.809, -6.746, 9)
-  for i in lista_imagenes:
-    Map.addLayer(ee.Image(i).multiply(0.0001), vis_rgb, f'Imagen {i[-2:]}') # .clip(roiDep)
-
+  if lista_imagenes[0][:4] == 'COPE':
+    for i in lista_imagenes:                   
+      Map.addLayer(ee.Image(i).multiply(0.0001), vis_rgb, f'Imagen {i[21:23]}/{i[23:25]}') # .clip(roiDep)
+  else:
+    for i in lista_imagenes:
+      Map.addLayer(ee.Image(i).multiply(0.0001), vis_rgb, f'Imagen {i[-4:-2]}/{i[-2:]}') # .clip(roiDep)
+    
   # Visualizar al último el distrito de Chongoyape
-  Map.addLayer(chongoyape, {'color':'00FF00'}, 'Chongoyape') # roiChongoyapeDraw
+  Map.addLayer(roi, {'color':'00FF00'}, 'Chongoyape') # roiChongoyapeDraw
   
   return Map
 
-# ------------- SENTINEL-2 -------------
-# Función: `generar_reporte_S2_SR(roi, year)`
-def generar_reporte_S2_SR(roi, year):
 
-  ID_snippet_name = 'COPERNICUS/S2_SR'
+# Función `maskS2clouds(image)` para enmascarar nubes en imágenes Sentinel-2
 
-  ## Filtrado de ee.imageCollection
-  icol_sr = (
-      ee.ImageCollection(ID_snippet_name)\
-      .filterDate(str(year), str(year+1))\
-      .filterBounds(roi)
-  )
-
-  ## Generando campos para la tabla
-  # Lista con fechas (en milisegundos)
-  lista_fechas = icol_sr.aggregate_array('system:time_start').getInfo()
-
-  # Lista con ID's
-  imgCol_ids = [f'{ID_snippet_name}/{i}' for i in icol_sr.aggregate_array("system:index").getInfo()]
-
-  # Tabla con pandas
-  # import pandas as pd
-
-  df = pd.DataFrame(lista_fechas, columns = ['millis'])
-
-  df['Sentinel-2 ID'] = imgCol_ids
-  df['Timestamp'] = pd.to_datetime(df['millis'], unit='ms')
-  df['Fecha Precisa'] = pd.DatetimeIndex(df['Timestamp']) # Con hora
-  df['Fecha Corta'] = pd.DatetimeIndex(df['Timestamp']).date
-  df['Año'] = pd.DatetimeIndex(df['Timestamp']).year
-  df['Mes'] = pd.DatetimeIndex(df['Timestamp']).month
-  df['Día'] = pd.DatetimeIndex(df['Timestamp']).day
-  df['Hora'] = pd.DatetimeIndex(df['Timestamp']).hour
-  df['Día Juliano'] = pd.DatetimeIndex(df['Timestamp']).dayofyear
-  # df['Sensor'] = 'OLI/TIRS'
-  
-  df["Zenith Angle"] = icol_sr.aggregate_array('MEAN_SOLAR_ZENITH_ANGLE').getInfo()
-  df["Porcentaje Nubes (%)"] = icol_sr.aggregate_array('CLOUDY_PIXEL_PERCENTAGE').getInfo()
-  
-  df = df.drop(columns=['millis', 'Timestamp'])
-  # df.to_csv('datos_2020_L8_SR.csv')
-
-  return df # ,icol_sr
-
-# @markdown Función `maskS2clouds(image)` para enmascarar nubes
-
-# Operadores de bit a bit
+## Operadores de bit a bit
 # Operador bit a bit desplazamiento a la izquierda <<
 # Little indian (los bits se cuentan de izq a derecha) y Big indian (los bits se cuentan de derecha a izq)
 def maskS2clouds(image):
@@ -124,28 +105,3 @@ def maskS2clouds(image):
            .And(qa.bitwiseAnd(cirrus_cloud).eq(0))
   clean_image = image.updateMask(mask)
   return clean_image
-
-# @markdown Función `visualizar_mes_S2_SR(mes, df, roi)`
-
-def visualizar_mes_S2_SR(mes, df, roi):
-  
-  # Del dataframe obtenemos las imagenes de acuerdo al mes que queramos
-  lista_imagenes = df[df['Mes'] == mes]['Sentinel-2 ID'].tolist()
-
-  # Parametros de visualización RGB
-  vis_rgb = {'min': 0.0, 'max': 0.3, 'bands': ['B4', 'B3', 'B2']} # 'gamma':1.4
-
-  # geemap para la visualización 
-  Map = geemap.Map(layer_ctrl=True) # basemap='HYBRID',OpenStreetMap.Mapnik
-  Map.centerObject(roi, 12)  #11, Map.setCenter(-79.809, -6.746, 9)
-
-  # Agregar imágenes por mes
-  # Visualizar al último el distrito de Chongoyape
-  Map.addLayer(roi, {'color':'00FF00'}, 'ROI') # roiChongoyapeDraw
-
-  for i in lista_imagenes:
-    Map.addLayer(maskS2clouds(ee.Image(i)).multiply(0.0001).clip(roi), 
-                 vis_rgb, f'Imagen {i[21:23]}/{i[23:25]} con máscara básica') # {i} .clip(roiChongoyape)
-    Map.addLayer(ee.Image(i).multiply(0.0001).clip(roi), vis_rgb, f'Imagen {i[21:23]}/{i[23:25]}') # {i} .clip(roiChongoyape)
-  
-  return Map
