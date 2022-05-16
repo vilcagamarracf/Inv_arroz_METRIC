83554db624b20b24d0a54e73c8b8490b64f4d0d6
Cesar Francisco Vilca Gamarra <60679623+vilcagamarracf@users.noreply.github.com>
2022-05-16 02:48:15 -0500

    Update

--- files/scripts/vis_tools.py
+++ files/scripts/vis_tools.py
@@ -1,8 +1,14 @@
 import ee
-import geemap
-import pandas as pd
 
-# @markdown Función `generar_reporte(icol)`
+import geemap
+from geemap import cartoee
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Función `generar_reporte(icol)`
 
 def generar_reporte(icol):
 
@@ -40,14 +46,24 @@ def generar_reporte(icol):
     # df['Hora'] = pd.DatetimeIndex(df['Timestamp']).hour
     df['Día Juliano'] = pd.DatetimeIndex(df['Timestamp']).dayofyear
 
-    l_icols_id = ['LANDSAT/LC08/C01/T1','LANDSAT/LC08/C02/T1', 'LANDSAT/LC08/C02/T1_TOA', 
-                    'LANDSAT/LC09/C02/T1','LANDSAT/LC09/C02/T1_L2', 'LANDSAT/LC09/C02/T1_TOA',
-                    'LANDSAT/LC09/C02/T1_RT_TOA',
-                    'LANDSAT/LE07/C02/T1', 'LANDSAT/LE07/C02/T1_TOA',
-                    'LANDSAT/LT05/C01/T1_TOA', 'LANDSAT/LM05/C01/T1']
+    l_icols_id = [
+        'LANDSAT/LC08/C01/T1', 'LANDSAT/LC08/C02/T1', 'LANDSAT/LC08/C02/T1_TOA', 
+        'LANDSAT/LC09/C02/T1','LANDSAT/LC09/C02/T1_L2', 'LANDSAT/LC09/C02/T1_TOA',
+        'LANDSAT/LC09/C02/T1_RT_TOA',
+        'LANDSAT/LE07/C02/T1', 'LANDSAT/LE07/C02/T1_TOA',
+        'LANDSAT/LT05/C01/T1_TOA', 'LANDSAT/LM05/C01/T1'
+        ]
 
-    l_icols_sr_id = ['LANDSAT/LT05/C02/T1_L2','LANDSAT/LC08/C02/T1_L2',
-                    'LANDSAT/LE07/C02/T1_L2']
+    l_icols_sr_id = [
+        'LANDSAT/LT05/C02/T1_L2',
+        'LANDSAT/LC08/C02/T1_L2',
+        'LANDSAT/LE07/C02/T1_L2'
+        ]
+
+    icol_s2 = [
+        'COPERNICUS/S2',
+        'COPERNICUS/S2_SR'
+        ]
 
     # Agregando propiedades
     if ID_snippet_name == 'LANDSAT/LC08/C01/T1_SR':
@@ -66,18 +82,19 @@ def generar_reporte(icol):
         df['ZENITH ANGLE'] = df['ZENITH ANGLE'].round(decimals=7)
         df = df.drop(columns=['SUN_ELEVATION'])
 
-    elif ID_snippet_name == 'COPERNICUS/S2_SR':
-        df['MEAN_SOLAR_ZENITH_ANGLE'] = icol.aggregate_array('MEAN_SOLAR_ZENITH_ANGLE').getInfo()
-        df['CLOUDY_PIXEL_PERCENTAGE'] = icol.aggregate_array('CLOUDY_PIXEL_PERCENTAGE').getInfo()
+    elif ID_snippet_name in icol_s2:
+        df['MEAN SOLAR ZENITH ANGLE'] = icol.aggregate_array('MEAN_SOLAR_ZENITH_ANGLE').getInfo()
+        df['CLOUDY PIXEL PERCENTAGE'] = icol.aggregate_array('CLOUDY_PIXEL_PERCENTAGE').getInfo()
     
     df = df.drop(columns=['millis', 'Timestamp'])
-    # df.to_csv('datos_2020_L8_SR.csv')
     
     return df
 
-# @markdown Función `ver_imgs_mensual(mes, df, snippet_name, roi)`
+
+# Función `ver_imgs_mensual(mes, df, snippet_name, roi)`
 
 def ver_imgs_mensual(mes, df, snippet_name, roi):
+
     """Visualizar imágenes a partir del reporte generado con la función generar_reporte
     
     Argumentos:
@@ -102,20 +119,31 @@ def ver_imgs_mensual(mes, df, snippet_name, roi):
     Map = geemap.Map(layer_ctrl=True) # basemap='OpenStreetMap.Mapnik', 
     Map.centerObject(roi, 11)  # Map.setCenter(-79.809, -6.746, 9)
 
-    icol_t1 = ['LANDSAT/LC08/C01/T1', 'LANDSAT/LC08/C02/T1', 
-                'LANDSAT/LC09/C02/T1']
+    icol_t1 = [
+        'LANDSAT/LC08/C01/T1', 
+        'LANDSAT/LC08/C02/T1', 
+        'LANDSAT/LC09/C02/T1'
+        ]
                     
-    icol_t1_l2 = ['LANDSAT/LC08/C02/T1_L2', 
-                    'LANDSAT/LC09/C02/T1_L2']
+    icol_t1_l2 = [
+        'LANDSAT/LC08/C02/T1_L2', 
+        'LANDSAT/LC09/C02/T1_L2'
+        ]
 
-    icol_le07_lt05 = ['LANDSAT/LT05/C02/T1_L2', 
-                        'LANDSAT/LE07/C02/T1_L2']
+    icol_le07_lt05 = [
+        'LANDSAT/LT05/C02/T1_L2', 
+        'LANDSAT/LE07/C02/T1_L2'
+        ]
 
-    icol_toa = ['LANDSAT/LC08/C02/T1_TOA',
-                'LANDSAT/LC09/C02/T1_TOA']
+    icol_toa = [
+        'LANDSAT/LC08/C02/T1_TOA',
+        'LANDSAT/LC09/C02/T1_TOA'
+        ]
+
+    icol_s2 = ['COPERNICUS/S2','COPERNICUS/S2_SR']
 
     # Sentinel-2
-    if snippet_name == 'COPERNICUS/S2_SR':
+    if snippet_name in icol_s2:
         for i in lista_imagenes:
             image = ee.Image(i).multiply(0.0001)                 
             Map.addLayer(image, vis_rgb, f'Imagen {i}')
@@ -171,3 +199,63 @@ def ver_imgs_mensual(mes, df, snippet_name, roi):
     Map.addLayer(empty, {}, 'ROI')
 
     return Map
+
+# Función `get_grafica_cartoee(image, vis_params, title_map, label, img_date, save_fig=None)`
+
+def get_grafica_cartoee(image, vis_params, title_map, label, save_fig=None):
+    
+    zoom_region = [-79.787761, -6.607186, -79.771781, -6.593991]
+
+    fig = plt.figure(figsize=(8,6))
+
+    # ee.Image a plotear
+    ax = cartoee.get_map(image, region=zoom_region, vis_params=vis_params)
+
+    # Añadir barra de color
+    # https://matplotlib.org/stable/api/colorbar_api.html#module-matplotlib.colorbar   
+    cb = cartoee.add_colorbar(
+        ax, 
+        vis_params=vis_params, 
+        loc='right', 
+        label=label, 
+        posOpts=[0.86, 0.25, 0.02, 0.5]
+    )# drawedges=True, extend='both')
+
+    # Añadir grillas
+    cartoee.add_gridlines(
+        ax, 
+        interval=0.005, 
+        ytick_rotation=90, 
+        linestyle=":", 
+        linewidth=0
+    ) # xtick_rotation=45
+
+    # Añadir texto
+    ax.text(-79.7828, -6.5962, title_map, fontsize=14)
+
+    # add scale bar
+    scale_bar_dict = {
+          "length": 100, 
+          "xy": (0.9, 0.05), 
+          "linewidth": 2,
+          "fontsize": 12,
+          "color": "black",
+          "unit": "m",
+          "ha": "center",
+          "va": "bottom",    
+    }
+    cartoee.add_scale_bar_lite(ax, **scale_bar_dict)
+    
+    ax.tick_params(axis = 'both', labelsize = 11)
+    
+    # Guardar graficas
+    if save_fig != None:
+        # plt.savefig(f'/Users/usuario/Downloads/{title_map}_{img_date}.jpg')
+        # plt.savefig(f'/content/{title_map}_{img_date}.jpg')
+        ruta = r'C:/Users/usuario/Documents/00-notebooks-2022/images/cartoee/'
+        ruta_img = ruta + save_fig + '.jpg'
+        plt.savefig(ruta_img)#, dpi=300)
+    else:
+        pass
+
+    plt.show()
